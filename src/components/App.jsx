12501bc6b21de7b4338e2a96189e2aaec7bb799b
AndreiKrai <okraichenko.a@gmail.com>
2022-12-09 12:30:19 +0200

    fixed warnings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,42 +5,39 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { addContact, deleteContact, fetchContacts } from 'redux/thunk';
 import { useEffect } from 'react';
 import { nanoid } from 'nanoid';
-import { selectContacts, selectFilter, selectfilteredArr, selectIsOpenToWork } from 'redux/selectors.phoneBook';
-
+import {
+  selectContacts,
+  // selectFilter,
+  selectfilteredArr,
+  // selectIsOpenToWork,
+} from 'redux/selectors.phoneBook';
 
 export const App = () => {
-
-
   const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-  const isOpenToWork = useSelector(selectIsOpenToWork);
+  // const filter = useSelector(selectFilter);
+  // const isOpenToWork = useSelector(selectIsOpenToWork);
   // селектори будуть перерендувати едементи, бо useSelector перевіряє попередній та поточні данні === ,а масиви та о'бєкти так будуть завжди false тому їх треба кешувати
   const filteredArr = useSelector(selectfilteredArr);
-
-  
-
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
   const handleRemoveContact = id => dispatch(deleteContact(id));
-  const handleSubmit = (name,number,isOpenToWork) => {
-    
+  const handleSubmit = (name, number, isOpenToWork) => {
     const isExist = contacts.find(user => user.name === name);
     if (!isExist) {
-      const newContact = {id: nanoid(),name ,isOpenToWork,phone:number};
+      const newContact = { id: nanoid(), name, isOpenToWork, phone: number };
       dispatch(addContact(newContact));
     } else alert(`${name} is already in contact`);
-    
   };
 
   // const filteredArr = contacts => {
   //   // цю функцію бажано перенести в файл із селекторами і повернути сюди тільки як селектор. тоді і селектори з- 14-17строки не будуть потрібні в цьому файлі
   //   const nameFilter = contacts.filter(user =>
   //     user.name.toLowerCase().includes(filter.toLowerCase())
-  //   );   
+  //   );
   //   if(isOpenToWork === "notSelected"){
   //     return nameFilter}
   //   else{
@@ -52,7 +49,7 @@ export const App = () => {
   return (
     <div className="form-check" style={style}>
       <h1>Phonebook</h1>
-      <ContactForm onSubmit={handleSubmit}/>
+      <ContactForm onSubmit={handleSubmit} />
       <h2>Contacts</h2>
       <Filter />
       <ContactList contacts={filteredArr} onClick={handleRemoveContact} />
@@ -60,46 +57,44 @@ export const App = () => {
   );
 };
 
+// const CONTACTS_LOCAL_STORAGE = 'contactList';
+// const contactLocalStorage = localStorage.getItem(CONTACTS_LOCAL_STORAGE);
 
+// const [contacts, setContacts] = useState([]);
+// const [filter, setFilter] = useState('');
 
-  // const CONTACTS_LOCAL_STORAGE = 'contactList';
-  // const contactLocalStorage = localStorage.getItem(CONTACTS_LOCAL_STORAGE);
+// useEffect(() => {
+//   localStorage.setItem(CONTACTS_LOCAL_STORAGE, JSON.stringify(contacts));
+// }, [contacts]);
 
-  // const [contacts, setContacts] = useState([]);
-  // const [filter, setFilter] = useState('');
+// console.log('contacts',contacts)
+// console.log('filter',filter)
 
-  // useEffect(() => {
-  //   localStorage.setItem(CONTACTS_LOCAL_STORAGE, JSON.stringify(contacts));
-  // }, [contacts]);
+// const handleAddContact = (name, number) => {
+//   const isExist = contacts.find(user => user.name === name);
+//   if (!isExist) {
+//     setContacts([...contacts, { id: nanoid(), name: name, number: number }]);
+//   } else alert(`${name} is already in contact`);
+// };
 
-  // console.log('contacts',contacts)
-  // console.log('filter',filter)
+// const handleChange = e => dispatch(setFilter(e.target.value));
+// const handleChange = e => setFilter(e.target.value);
 
-  // const handleAddContact = (name, number) => {
-  //   const isExist = contacts.find(user => user.name === name);
-  //   if (!isExist) {
-  //     setContacts([...contacts, { id: nanoid(), name: name, number: number }]);
-  //   } else alert(`${name} is already in contact`);
-  // };
+// handleChange = e => {
+//   const { name } = e.target;
+//   this.setState({ [name]: e.target.value });
+// };
 
-  // const handleChange = e => dispatch(setFilter(e.target.value));
-  // const handleChange = e => setFilter(e.target.value);
+// ------------тимчасово
+// const handleRemoveContact = id => dispatch(removeContact(id));
+// const handleRemoveContact = id =>
+//   setContacts(contacts.filter(contact => contact.id !== id));
+// ---------------------
 
-  // handleChange = e => {
-  //   const { name } = e.target;
-  //   this.setState({ [name]: e.target.value });
-  // };
-
-  // ------------тимчасово
-  // const handleRemoveContact = id => dispatch(removeContact(id));
-  // const handleRemoveContact = id =>
-  //   setContacts(contacts.filter(contact => contact.id !== id));
-  // ---------------------
-
-  // handleRemoveContact = id => {
-  //   this.setState(prevState => {
-  //     return {
-  //       contacts: prevState.contacts.filter(contact => contact.id !== id),
-  //     };
-  //   });
-  // };
+// handleRemoveContact = id => {
+//   this.setState(prevState => {
+//     return {
+//       contacts: prevState.contacts.filter(contact => contact.id !== id),
+//     };
+//   });
+// };
